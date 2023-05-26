18bed6f1f32d47052459e4cddd0a1b49856581e7
ehize <ehize18@gmail.com>
2023-05-26 22:49:18 +0500

    Refactoring

--- gamer/Game1.cs
+++ gamer/Game1.cs
@@ -12,8 +12,8 @@ namespace gamer
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
-        private List<Button> _buttons;
-        private List<TextField> _texts;
+        private List<Button> _upgradeButtons;
+        private List<TextField> _characterDescriptions;
         private List<Character> _characters;
         private int _level = 1;
         private Enemy _enemy;
@@ -66,18 +66,98 @@ namespace gamer
             var font24 = Content.Load<SpriteFont>("disket24");
             var font18 = Content.Load<SpriteFont>("disket18");
             var font14 = Content.Load<SpriteFont>("disket14");
+
             _nextLevelButton = new Button(buttonTexture, font14, -2)
             {
                 Text = "next\nlevel",
                 Position = new Vector2(-1000, 0)
             };
             _nextLevelButton.Click += IncreaseLevel;
+
             var mcUpgradeButton = new Button(buttonTexture, font14, -1)
             {
                 Position = new Vector2(515, 286),
                 Text = "UPGRADE"
             };
             mcUpgradeButton.Click += IncreaseMCDamage;
+
+            _enemy = new Enemy(_level, font72, enemyTextures, _mcDamage)
+            {
+                Position = new Vector2(826, 200)
+            };
+            _enemy.Die += IncreaseMoney;
+            _enemy.BossNotKilled += DecreaseLevel;
+
+            _characters = new List<Character>()
+            {
+                new Character(0, Content.Load<Texture2D>("diluc"))
+                {
+                    Position = new Vector2(36, 407)
+                },
+                new Character(1, Content.Load<Texture2D>("diona"))
+                {
+                    Position = new Vector2(36, 526)
+                },
+                new Character(2, Content.Load<Texture2D>("zhongli"))
+                {
+                    Position = new Vector2(36, 645)
+                },
+                new Character(3, Content.Load<Texture2D>("hutao"))
+                {
+                Position = new Vector2(36, 764)
+                }
+            };
+            _characters.ForEach(c => c.Damage += CharacterDamage);
+            _characterDescriptions = new List<TextField>
+            {
+                new TextField(font18, "main character\nincrease click\ndamage")
+                {
+                    Position = new Vector2(149, 302)
+                },
+                new TextField(font18, "diluc\nincrease idle\ndamage on 1")
+                {
+                    Position = new Vector2(149, 421)
+                },
+                new TextField(font18, "diona\nincrease idle\ndamage on 10")
+                {
+                    Position = new Vector2(149, 540)
+                },
+                new TextField(font18, "zhongli\nincrease idle\ndamage on 100")
+                {
+                    Position = new Vector2(149, 659)
+                },
+                new TextField(font18, "hutao\nincrease idle\ndamage on 1000")
+                {
+                    Position = new Vector2(149, 778)
+                }
+            };
+
+            _upgradeButtons = new List<Button>()
+            {
+                mcUpgradeButton,
+                new Button(buttonTexture, font14, 0)
+                {
+                    Position = new Vector2(515, 405),
+                    Text = "UPGRADE"
+                },
+                new Button(buttonTexture, font14, 1)
+                {
+                    Position = new Vector2(515, 525),
+                    Text = "UPGRADE"
+                },
+                new Button(buttonTexture, font14, 2)
+                {
+                    Position = new Vector2(515, 645),
+                    Text = "UPGRADE"
+                },
+                new Button(buttonTexture, font14, 3)
+                {
+                    Position = new Vector2(515, 765),
+                    Text = "UPGRADE"
+                }
+            };
+            _upgradeButtons.ForEach(c => c.Click += UpgradeCharacter);
+
             _moneyText = new TextField(font72, _money.ToString())
             {
                 Position = new Vector2(40, 26),
@@ -86,104 +166,10 @@ namespace gamer
             {
                 Position = new Vector2(40, 160)
             };
-            _enemy = new Enemy(_level, font72, enemyTextures, _mcDamage)
-            {
-                Position = new Vector2(826, 200)
-            };
-            _enemy.Die += IncreaseMoney;
-            var diluc = new Character(0, Content.Load<Texture2D>("diluc"))
-            {
-                Position = new Vector2(36, 407)
-            };
-            diluc.Damage += CharacterDamage;
-            var diona = new Character(1, Content.Load<Texture2D>("diona"))
-            {
-                Position = new Vector2(36, 526)
-            };
-            diona.Damage += CharacterDamage;
-            var zhongli = new Character(2, Content.Load<Texture2D>("zhongli"))
-            {
-                Position = new Vector2(36, 645)
-            };
-            zhongli.Damage += CharacterDamage;
-            var hutao = new Character(3, Content.Load<Texture2D>("hutao"))
-            {
-                Position = new Vector2(36, 764)
-            };
-            hutao.Damage += CharacterDamage;
-            _characters = new List<Character>()
-            {
-                diluc,
-                diona,
-                zhongli,
-                hutao
-            };
-            var dilucUpgradeButton = new Button(buttonTexture, font14, 0)
-            {
-                Position = new Vector2(515, 405),
-                Text = "UPGRADE",
-                Text2 = _characters[0].UpgradeCost.ToString()
-            };
-            var dionaUpgradeButton = new Button(buttonTexture, font14, 1)
-            {
-                Position = new Vector2(515, 525),
-                Text = "UPGRADE"
-            };
-            var zhongliUpgradeButton = new Button(buttonTexture, font14, 2)
-            {
-                Position = new Vector2(515, 645),
-                Text = "UPGRADE"
-            };
-            var hutaoUpgradeButton = new Button(buttonTexture, font14, 3)
-            {
-                Position = new Vector2(515, 765),
-                Text = "UPGRADE"
-            };
-            dilucUpgradeButton.Click += UpgradeCharacter;
-            dionaUpgradeButton.Click += UpgradeCharacter;
-            zhongliUpgradeButton.Click += UpgradeCharacter;
-            hutaoUpgradeButton.Click += UpgradeCharacter;
-            _buttons = new List<Button>()
-            {
-                mcUpgradeButton,
-                dilucUpgradeButton,
-                dionaUpgradeButton,
-                zhongliUpgradeButton,
-                hutaoUpgradeButton
-            };
             _idleDamageText = new TextField(font24, "idle damage: " + GetIdleDamage(_characters))
             {
                 Position = new Vector2(40, 210)
             };
-            var mcDescription = new TextField(font18, "main character\nincrease click\ndamage")
-            {
-                Position = new Vector2(149, 302)
-            };
-            var dilucDescription = new TextField(font18, "diluc\nincrease idle\ndamage on 1")
-            {
-                Position = new Vector2(149, 421)
-            };
-            var dionaDescription = new TextField(font18, "diona\nincrease idle\ndamage on 10")
-            {
-                Position = new Vector2(149, 540)
-            };
-            var zhongliDescription = new TextField(font18, "zhongli\nincrease idle\ndamage on 100")
-            {
-                Position = new Vector2(149, 659)
-            };
-            var hutaoDescription = new TextField(font18, "hutao\nincrease idle\ndamage on 1000")
-            {
-                Position = new Vector2(149, 778)
-            };
-            _texts = new List<TextField>
-            {
-                mcDescription,
-                dilucDescription,
-                dionaDescription,
-                zhongliDescription,
-                hutaoDescription
-            };
-            _enemy.BossNotKilled += DecreaseLevel;
         }
 
 
@@ -251,14 +237,14 @@ namespace gamer
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
-            foreach (var button in _buttons)
+            foreach (var button in _upgradeButtons)
                 if (button.ID != -1)
                     button.Update(gameTime, _characters[button.ID].UpgradeCost.ToString());
                 else
                     button.Update(gameTime, _mcUpgradeCost.ToString());
             _nextLevelButton.Update(gameTime, null);
-            foreach (var text in _texts)
-                text.Update(gameTime, null);
+            foreach (var description in _characterDescriptions)
+                description.Update(gameTime, null);
             _moneyText.Update(gameTime, _money.ToString());
             _mcDamageText.Update(gameTime, "click damage: " + _mcDamage.ToString());
             _idleDamageText.Update(gameTime, "idle damage: " + GetIdleDamage(_characters));
@@ -283,9 +269,9 @@ namespace gamer
             _spriteBatch.Draw(Content.Load<Texture2D>("mc"), new Vector2(36, 288), Color.White);
             foreach (var character in _characters)
                 character.Draw(gameTime, _spriteBatch);
-            foreach (var button in _buttons)
+            foreach (var button in _upgradeButtons)
                 button.Draw(gameTime, _spriteBatch);
-            foreach (var text in _texts)
+            foreach (var text in _characterDescriptions)
                 text.Draw(gameTime, _spriteBatch);
             _moneyText.Draw(gameTime, _spriteBatch);
             _mcDamageText.Draw(gameTime, _spriteBatch);
